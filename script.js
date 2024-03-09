92e74be4152387f6a3c58c6e568f2b93b3a01c3f
Aptomo2 <andangpratomo02@gmail.com>
2024-03-09 21:29:52 +0700

    Finished

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ window.onscroll = () => {
             navLink.forEach(link => {
                 link.classList.remove('active');
             });
-            document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');          
+            document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
         };
     });
 
@@ -44,13 +44,58 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
+// Scroll reveal animation
+
 ScrollReveal({
     distance: '80px',
     duration: 2000,
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .porto-box, .contact form', {origin: 'buttom'});
-ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .porto-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+// Handle form to google sheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyo1ef97orvZpOmmdTZpqR4Z_BIlTpqrmH4c1GM5MhgXkbexNmCjoueqeaC3MYxirQgwQ/exec';
+const form = document.forms['submit-to-google-sheet'];
+const btnSend = document.querySelector('.btn-send');
+const buttonLoad = document.querySelector('.buttonload');
+const myAlert = document.querySelector('.alert');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Tampilkan button "Loading" dan sembunyikan button "Send Message"
+    buttonLoad.style.display = 'inline-block';
+    btnSend.style.display = 'none';
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            console.log('Success!', response);
+
+            // Sembunyikan button "Loading" setelah berhasil
+            buttonLoad.style.display = 'none';
+            btnSend.style.display = 'inline-block';
+
+            // Kosongkan input form
+            form.reset();
+
+            // Tampilkan alert setelah berhasil
+            myAlert.style.display = 'block';
+
+            // Hilangkan alert setelah beberapa detik
+            setTimeout(function () {
+                myAlert.style.display = 'none';
+            }, 5000); // Tampilkan alert selama 5 detik
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+
+            // Sembunyikan button "Loading" dan kembalikan button "Send Message"
+            buttonLoad.style.display = 'none';
+            btnSend.style.display = 'inline-block';
+        });
+});
