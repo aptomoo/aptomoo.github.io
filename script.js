cf9f0984bb8aa9a1e3543c22c74dfb938e6078d4
Aptomo2 <andangpratomo02@gmail.com>
2024-03-10 16:33:07 +0700

    hmmmm

--- script.js
+++ script.js
@@ -85,3 +85,14 @@ form.addEventListener('submit', e => {
             btnSend.style.display = 'inline-block';
         });
 });
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .porto-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
